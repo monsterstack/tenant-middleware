60623424523b1a97a9b9d2c97a0b16110cdec7b4
zacharyrote <zachary.rote@gmail.com>
2017-02-13 16:28:48 -0500

    multi tenant db lookup

--- libs/tenant.js
+++ libs/tenant.js
@@ -1,15 +1,23 @@
 'use strict';
 const TENANT_NAME_KEY = "TenantName";
+const multiTenantDb = require('multi-tenancy-db');
 
 class TenantDBCreationMiddleware {
   constructor() {
 
   }
 
-  tenantDb(modleFactory) {
+  tenantDb(modelFactory) {
     return (req, res, next) => {
       // Build DB Connection and hang on request.
       let tenantName = req.headers[TENANT_NAME_KEY];
+
+      // This should be async.
+      let db = multiTenantDb.findOrCreateNewConnection(tenantName, modelFactory);
+      if(db) {
+        req.db = db;
+      }
+
       next();
     }
   }
